3de3eeb48b0d2e2073123238994587246f62bed6
Kyle Lewis <kyle@spectacledbear.ca>
2026-04-18 18:02:07 -0400

    Adding OLED display first try
    - Adding CircuitPython libraries for SHT1107 OLED display and display_text for displayio.
    - Updating bme280.py with rough implementation of displaying temperature and humidity on the OLED display.
    - Updated README with resources used.

--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 
 **Adafruit ESP32 Feather V2.**
 
+**Adafruit 128x64 OLED FeatherWing.**
+
 **Thonny.** A lightweight Python IDE that offers debugging and file utilities for my ESP32 device. This is how I transfer files and run scripts on the ESP32 device.
 
-**CircuitPython**.
+**CircuitPython.**
 I used the [CircuitPython made for the Adafruit Feather ESP32 V2](https://circuitpython.org/board/adafruit_feather_esp32_v2/).
 Specifically [this one](https://downloads.circuitpython.org/bin/adafruit_feather_esp32_v2/en_US/adafruit-circuitpython-adafruit_feather_esp32_v2-en_US-10.0.3.bin). I've included this binary file in the repo, for archival purposes.
 
@@ -14,6 +16,9 @@ Specifically [this one](https://downloads.circuitpython.org/bin/adafruit_feather
 The [BME280 library](https://github.com/adafruit/Adafruit_CircuitPython_BME280) and the [BusDevice library](https://github.com/adafruit/Adafruit_CircuitPython_BusDevice) are used.
 Alternatively you could use the comprehensive [CircuitPython driver bundle](https://github.com/adafruit/Adafruit_CircuitPython_Bundle).
 The required libraries are included in the `lib/` directory.
+
+**Adafruit DisplayIO SH1107 CircuitPython library.**
+The [SH1107 library](https://github.com/adafruit/Adafruit_CircuitPython_DisplayIO_SH1107) is used to drive the OLED FeatherWing and the [display_text library](https://github.com/adafruit/Adafruit_CircuitPython_Display_Text) is a helper library for displaying text using CircuitPython's displayio library.
 
 ## Setup
 
@@ -23,4 +28,4 @@ I used ESP-IDF and its esptool.py to flash CircuitPython onto the device.
 
     esptool.py --port <COM_PORT> write_flash -z 0x0 adafruit-circuitpython-adafruit_feather_esp32_v2-en_US-10.0.3.bin
 
-Then copied the `adafruit_bme280` and `adafruit_bus_device` libraries from the `lib/` directory to the ESP32 device using Thonny.
+Then copied the contents of the `lib/` directory to the root directory of the ESP32 device using the Thonny program.
--- bme280.py
+++ bme280.py
@@ -5,28 +5,62 @@
 
 import time
 
+import adafruit_displayio_sh1107
 import board
-
+import displayio
+import terminalio
 from adafruit_bme280 import basic as adafruit_bme280
+
+from adafruit_display_text import label
+from i2cdisplaybus import I2CDisplayBus
+
+displayio.release_displays()
 
 # Create sensor object, using the board's default I2C bus.
 # i2c = board.I2C()  # uses board.SCL and board.SDA
-i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+i2c = (
+    board.STEMMA_I2C()
+)  # For using the built-in STEMMA QT connector on a microcontroller
+
+# SH1107 is vertically oriented 64x128
+WIDTH = 128
+HEIGHT = 64
+BORDER = 2
+display_bus = I2CDisplayBus(i2c, device_address=0x3C)
+display = adafruit_displayio_sh1107.SH1107(display_bus, width=WIDTH, height=HEIGHT)
+display.auto_refresh = True
+# Make the display context
+splash = displayio.Group()
+display.root_group = splash
+
+text1 = "T:  0.0 C"  # overly long to see where it clips
+text_area = label.Label(terminalio.FONT, text=text1, color=0xFFFFFF, x=8, y=8)
+splash.append(text_area)
+text2 = "RH: 0.00 %%"
+text_area2 = label.Label(terminalio.FONT, text=text2, color=0xFFFFFF, x=8, y=20)
+splash.append(text_area2)
+
+
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-
-# OR create sensor object, using the board's default SPI bus.
-# import digitalio
-# spi = board.SPI()
-# bme_cs = digitalio.DigitalInOut(board.D10)
-# bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
-
 # change this to match the location's pressure (hPa) at sea level
-bme280.sea_level_pressure = 1013.25
+bme280.sea_level_pressure = 1010.0  # 2026-04-16T20:00-04:00
 
 while True:
+    temperature = bme280.temperature
+    relative_humidity = bme280.relative_humidity
+    pressure = bme280.pressure
+    altitude = bme280.altitude
+
     # print("\nTemperature: %0.1f C" % bme280.temperature)
     # print("Humidity: %0.1f %%" % bme280.relative_humidity)
     # print("Pressure: %0.1f hPa" % bme280.pressure)
     # print("Altitude = %0.2f meters" % bme280.altitude)
-    print(f"{bme280.temperature:>7.2f} C   {bme280.relative_humidity:>6.2f} % RH   {(bme280.pressure / 10):>6.2f} kPa")
+
+    print(
+        f"{temperature:>7.2f} C   {relative_humidity:>6.2f} % RH   {(pressure / 10):>6.2f} kPa"
+    )
+
+    text_area.text = "T:  %0.1f C" % temperature
+    text_area2.text = "RH: %0.2f %%" % relative_humidity
+
     time.sleep(60)
