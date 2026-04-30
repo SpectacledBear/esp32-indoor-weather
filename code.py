51827f403cc32ea245fbaad4e5fd37b6924a7ddd
Kyle Lewis <kyle@spectacledbear.ca>
2026-04-30 10:14:33 -0400

    Separating display code

--- code.py
+++ code.py
@@ -1,34 +1,13 @@
 import time
 
-import adafruit_displayio_sh1107
 import adafruit_sht4x
 import board
-import displayio
-import terminalio
-from adafruit_display_text import label
-from i2cdisplaybus import I2CDisplayBus
 
-displayio.release_displays()
+from display import Display
 
 i2c = board.STEMMA_I2C()
 
-# SH1107 is vertically oriented 64x128
-WIDTH = 128
-HEIGHT = 64
-BORDER = 2
-display_bus = I2CDisplayBus(i2c, device_address=0x3C)
-display = adafruit_displayio_sh1107.SH1107(display_bus, width=WIDTH, height=HEIGHT)
-display.auto_refresh = True
-# Make the display context
-splash = displayio.Group()
-display.root_group = splash
-
-text1 = "T:  0.0 C"  # overly long to see where it clips
-text_area = label.Label(terminalio.FONT, text=text1, color=0xFFFFFF, x=8, y=8)
-splash.append(text_area)
-text2 = "RH: 0.00 %%"
-text_area2 = label.Label(terminalio.FONT, text=text2, color=0xFFFFFF, x=8, y=20)
-splash.append(text_area2)
+display = Display(i2c)
 
 sht = adafruit_sht4x.SHT4x(i2c)
 print("Found SHT4x with serial number", hex(sht.serial_number))
@@ -38,7 +17,7 @@ while True:
     temperature, relative_humidity = sht.measurements
     print(f"{temperature:>7.2f} C   {relative_humidity:>6.2f} % RH")
 
-    text_area.text = "T:  %0.1f C" % temperature
-    text_area2.text = "RH: %0.2f %%" % relative_humidity
+    display.set_temperature(temperature)
+    display.set_relative_humidity(relative_humidity)
 
     time.sleep(60)
---
+++ display.py
@@ -0,0 +1,34 @@
+import adafruit_displayio_sh1107
+import displayio
+import terminalio
+from adafruit_display_text import label
+from i2cdisplaybus import I2CDisplayBus
+
+# SH1107 is vertically oriented 64x128
+WIDTH = 128
+HEIGHT = 64
+BORDER = 2
+
+displayio.release_displays()
+
+class Display:
+    def __init__(self, i2c):
+        self.display_bus = I2CDisplayBus(i2c, device_address=0x3C)
+        self.display = adafruit_displayio_sh1107.SH1107(self.display_bus, width=WIDTH, height=HEIGHT)
+        self.display.auto_refresh = True
+        # Make the display context
+        self.splash = displayio.Group()
+        self.display.root_group = self.splash
+
+        text1 = "T:  0.0 C"  # overly long to see where it clips
+        self.text_area = label.Label(terminalio.FONT, text=text1, color=0xFFFFFF, x=8, y=8)
+        self.splash.append(self.text_area)
+        text2 = "RH: 0.00 %%"
+        self.text_area2 = label.Label(terminalio.FONT, text=text2, color=0xFFFFFF, x=8, y=20)
+        self.splash.append(self.text_area2)
+
+    def set_temperature(self, temperature):
+        self.text_area.text = "T:  %0.1f C" % temperature
+
+    def set_relative_humidity(self, relative_humidity):
+        self.text_area2.text = "RH: %0.2f %%" % relative_humidity
